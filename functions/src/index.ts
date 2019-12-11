02791a5d8f9e440143369b8d623d7b3a71931db7
Clément DOUIN <soywod@users.noreply.github.com>
2019-12-11 14:44:49 +0100

    add token exp 30 min

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,7 +5,7 @@ import * as jwt from "jsonwebtoken"
 admin.initializeApp()
 
 const JWT_SECRET = String(process.env.JWT_SECRET)
-const func = functions.region("europe-west1").https.onCall
+const EXP_DELAY = 60 * 60 * 30 // 30 min
 
 type GenerateToken = (_: GenerateTokenInput) => Promise<GenerateTokenOutput>
 type GenerateTokenInput = {idToken: string}
@@ -14,10 +14,13 @@ type GenerateTokenOutput = {ok: true; token: string} | {ok: false; message: stri
 const generateTokenFunc: GenerateToken = async ({idToken}) => {
   try {
     await admin.auth().verifyIdToken(idToken)
-    return {ok: true, token: jwt.sign({role: "bo_user"}, JWT_SECRET, {algorithm: "HS256"})}
+    const payload = {role: "bo_user", exp: Math.round(Date.now() / 1000) + EXP_DELAY}
+    const opts = {algorithm: "HS256"}
+    return {ok: true, token: jwt.sign(payload, JWT_SECRET, opts)}
   } catch (err) {
     return {ok: false, message: err.message}
   }
 }
 
-export const generateToken = func(generateTokenFunc)
+const onCall = functions.region("europe-west1").https.onCall
+export const generateToken = onCall(generateTokenFunc)
