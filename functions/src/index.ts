4002ca93029988a3ba3337191c1cc47e38902b07
Clément DOUIN <soywod@users.noreply.github.com>
2019-12-12 16:26:43 +0100

    remove confusing chars from pairing codes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,23 +8,26 @@ admin.initializeApp()
 const auth = admin.auth()
 const firestore = admin.firestore()
 const {onCall} = functions.region("europe-west1").https
-const codeCharRange = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+const codeCharRange = "ABCDEFGHJKMNPQRSTUVWXYZ123456789"
 
 function generatePairingCode() {
   try {
     const code: string = pipe([split(""), shuffle, take(5), join("")])(codeCharRange)
-    const codeDocRef = firestore.collection("pairingCodes").doc(code)
-    return firestore.runTransaction(async t => {
-      const codeDoc = await t.get(codeDocRef)
-      if (codeDoc.exists) throw new Error("code-already-exists")
-      t.set(codeDocRef, {
+    const screenRef = firestore.collection("screens").doc(code)
+    const {id} = firestore.collection("screens").doc()
+    return firestore.runTransaction(async tx => {
+      const screen = await tx.get(screenRef)
+      if (screen.exists) throw new Error("code-already-exists")
+      tx.set(screenRef, {
+        id,
         code,
+        userId: null,
         exp: DateTime.local()
           .plus({hour: 1})
           .toJSDate(),
       })
 
-      return {ok: true, code}
+      return {ok: true, id, code}
     })
   } catch (err) {
     return {ok: false, message: err.message}
@@ -44,27 +47,25 @@ export const requestPairingCode = onCall(async () => {
 export const linkScreenToUser = onCall(async ({idToken, code}) => {
   try {
     const {uid: userId} = await auth.verifyIdToken(idToken)
-    const codeDocRef = firestore.collection("pairingCodes").doc(code)
-    const screenId = await firestore.runTransaction(async t => {
-      const codeDoc = await t.get(codeDocRef)
-      if (!codeDoc.exists) throw new Error("code-not-found")
-      const data = codeDoc.data()
+    const screenRef = firestore.collection("screens").doc(code)
+    const screenId = await firestore.runTransaction(async tx => {
+      const screen = await tx.get(screenRef)
+      if (!screen.exists) throw new Error("code-not-found")
+      const data = screen.data()
       if (!data) throw new Error("code-invalid")
-
-      return firestore
-        .collection(`users/${userId}/screens`)
-        .add({code})
-        .then(({id}) => id)
+      if (data.userId) throw new Error("code-already-used")
+      tx.set(screenRef, {userId}, {merge: true})
+      return data.id
     })
 
-    // Clean expired codes
+    await firestore.collection(`users/${userId}/screens`).add({id: screenId, layoutId: null})
+
+    // Clean expired codes asynchronously
     firestore
-      .collection("pairingCodes")
+      .collection("screens")
       .where("exp", "<", new Date())
       .get()
       .then(snapshot => snapshot.forEach(doc => doc.ref.delete()))
-
-    await codeDocRef.delete()
 
     return {ok: true, screenId}
   } catch (err) {
